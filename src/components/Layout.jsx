231b55e5995925a4a25447c28a221bee4d9c0629
Julian Rousseau <julianrousseau31@gmail.com>
2021-09-10 23:58:35 -0300

    fix codigo repetido

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,7 @@ import data from "../helpers/data.json";
 
 const opcionesElegidas = [];
 let contador = 1;
+let op = "";
 
 export default class Layout extends Component {
   state = {
@@ -23,30 +24,17 @@ export default class Layout extends Component {
     opcionesElegidas.push(this.state.ultimaElegida);
   }
 
-  handleClickA = () => {
+  handleClick = (evento) => {
+    op = evento.target.id === "A" ? "a" : "b";
     contador = contador + 1;
     if (contador >= 6) {
       alert("FIN");
     } else {
       this.setState({
-        id: `${contador}a`,
-        historia: data.find((e) => e.id === `${contador}a`).historia,
-        opciones: data.find((e) => e.id === `${contador}a`).opciones,
-        ultimaElegida: "A",
-      });
-    }
-  };
-
-  handleClickB = () => {
-    contador = contador + 1;
-    if (contador >= 6) {
-      alert("FIN");
-    } else {
-      this.setState({
-        id: `${contador}b`,
-        historia: data.find((e) => e.id === `${contador}b`).historia,
-        opciones: data.find((e) => e.id === `${contador}b`).opciones,
-        ultimaElegida: "B",
+        id: `${contador}${op}`,
+        historia: data.find((e) => e.id === `${contador}${op}`).historia,
+        opciones: data.find((e) => e.id === `${contador}${op}`).opciones,
+        ultimaElegida: evento.target.id,
       });
     }
   };
@@ -57,8 +45,7 @@ export default class Layout extends Component {
         <h1 className="historia">{this.state.historia}</h1>
         <Opciones
           opciones={this.state.opciones}
-          handleClickA={this.handleClickA}
-          handleClickB={this.handleClickB}
+          handleClick={this.handleClick}
         />
         <Recordatorio
           ultimaElegida={this.state.ultimaElegida}
--- src/components/Opciones.jsx
+++ src/components/Opciones.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Opciones = ({ opciones, handleClickA, handleClickB }) => {
+const Opciones = ({ opciones, handleClick }) => {
   return (
     <div className="opciones">
       <div className="opcion">
-        <button id="A" className="botones" onClick={handleClickA}>
+        <button id="A" className="botones" onClick={handleClick}>
           A
         </button>
         <h2>{opciones.a}</h2>
       </div>
 
       <div className="opcion">
-        <button id="B" className="botones" onClick={handleClickB}>
+        <button id="B" className="botones" onClick={handleClick}>
           B
         </button>
         <h2>{opciones.b}</h2>
